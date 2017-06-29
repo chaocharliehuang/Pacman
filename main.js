36c17899abc9fccd3a8359b195c273796a665a21
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-06-29 11:18:10 -0700

    pacman now can't leave the maze or go through walls

--- main.js
+++ main.js
@@ -53,16 +53,16 @@ displayPacman();
 displayScore();
 
 document.onkeydown = function(e) {
-    if (e.keyCode === 37) { // left
+    if (e.keyCode === 37 && world[pacman.y][pacman.x-1] !== 2) { // left
         pacman.x--;
         $('div#pacman').css('transform', 'scaleX(1)');
-    } else if (e.keyCode === 39) { // right
+    } else if (e.keyCode === 39 && world[pacman.y][pacman.x+1] !== 2) { // right
         pacman.x++;
         $('div#pacman').css('transform', 'scaleX(-1)');
-    } else if (e.keyCode === 38) { // up
+    } else if (e.keyCode === 38 && world[pacman.y-1][pacman.x] !== 2) { // up
         pacman.y--;
         $('div#pacman').css('transform', 'rotate(90deg)');
-    } else if (e.keyCode === 40) { // down
+    } else if (e.keyCode === 40 && world[pacman.y+1][pacman.x] !== 2) { // down
         pacman.y++;
         $('div#pacman').css('transform', 'rotate(-90deg)');
     }
